44209d6e1248cd0722f7ca616b510e386d02ecd1
Prof2807 <proffessors2807@gmail.com>
2026-02-02 00:13:15 +0530

    test

--- final.js
+++ final.js
@@ -1,10 +1,11 @@
 let num = 24
 
 // comment
-
+// debug hahahah
 console.log("keeping log"); // keep
 
     console.log("indented log"); // keep
     // indent comment
+    // debug another comment
 
-
+console.log("Debuging log");
