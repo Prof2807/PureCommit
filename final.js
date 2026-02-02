c6a3035cc07436bfcc488e989e0fec6d006b43e9
Prof2807 <proffessors2807@gmail.com>
2026-02-02 17:52:18 +0530

    test

--- final.js
+++ final.js
@@ -1,7 +1,7 @@
 let API_KEY = 'pk_test_51H8jY2SDb5KJf3...';
 
-console.log("Final PureCommit version running with API Key:" ) // Keep 
-console.log("Debugging" , API_KEY)
-console.log("Debugging" , API_KEY) //    keep
+console.log("Final PureCommit version running with API Key:" ) 
+
+console.log("Keep" , API_KEY)
 // comment
-// debUG comment
+
